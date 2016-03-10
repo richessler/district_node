65df132f7c5b3913d680197e9b07222cca690a92
runtropolis <matt3r@gmail.com>
2016-03-10 17:52:43 -0500

    set up dc-api call and show results

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var express = require('express')
   , routes = require('./routes')
   , http = require('http')
   , path = require('path')
+  , request = require('request')
   , bodyParser = require('body-parser')
   , favicon = require('serve-favicon')
   , logger = require('morgan')
@@ -19,6 +20,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(favicon(__dirname + '/public/images/favicon.png'));
 app.use(logger('dev'));
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
 app.use(require('stylus').middleware(__dirname + '/public'));
@@ -28,7 +30,49 @@ if (app.get('env') == 'development') {
 	app.locals.pretty = true;
 }
 
-app.get('/', routes.index);
+var type = "events"
+  , params = ""
+  , url = "http://dc-api-test.herokuapp.com/api/v1/" + type + params;
+
+/**
+* Init
+*/
+
+// request(url, function(req, res, body) {
+//   res = JSON.parse(body);
+//   // console.log(res.events[0])
+//   }
+// );
+app.get('/', function(req, res, next) {
+  request(url, function(err, response, body) {
+    var r = JSON.parse(body);
+    data = r.events
+    console.log(data)
+    res.render('index', data)
+  })
+});
+
+
+// app.use(function(req, res, next){
+//   res.locals.items = "Value";
+//   next();
+// })
+
+
+
+
+
+
+// request(url, function (error, response, body) {
+//   if (!error && response.statusCode == 200) {
+//     var response = JSON.parse(body);
+//     console.log("Got a response: ", response);
+//   } else {
+//     console.log("Got an error: ", error, ", status code: ", response.statusCode);
+//   }
+// });
+
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
