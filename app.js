f006ced8b5a93e98962d15a84683aeda5d3049d2
runtropolis <matt3r@gmail.com>
2016-03-14 10:00:31 -0400

    remove moment

--- app.js
+++ app.js
@@ -23,7 +23,6 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
-app.locals.moment = require('moment')
 app.use(require('stylus').middleware(__dirname + '/public'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(function (req, res, next) {
